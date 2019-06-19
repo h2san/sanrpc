3ce204810ef896928a0d8d2ab330456cfcfe52bc
h2san <wyguoqingshan@163.com>
2019-06-19 23:20:49 +0800

    ci

--- tool/gencode/protoc-gen-sanrpc-client/main.go
+++ tool/gencode/protoc-gen-sanrpc-client/main.go
@@ -10,32 +10,42 @@ var formatPart1 = `package client
 
 import (
 	"context"
+
+	"github.com/hillguo/sanrpc/client/selector"
+	"github.com/hillguo/sanrpc/client/servicediscovery"
+	 xclient "github.com/hillguo/sanrpc/client"
 	"github.com/hillguo/sanrpc/client"
 	pb "%s/pb"
 )
 
 type %sClient struct {
-	client.XClient
+	xclient.XClient
 }
 
 func New%sClient() *%sClient {
-	client := %sClient{}
-	return &client
+	c := NoteSvrClient{}
+	c.XClient= xclient.NewXClient(
+		"%s",
+		xclient.Failtry,
+		selector.RoundRobin,
+		servicediscovery.NewP2PDiscovery("127.0.0.1:8080",""),
+		xclient.DefaultOption)
+	return &c
 }
 `
 
 var formatFunc = `
 func (c *%sClient) %s(ctx *context.Context, req *pb.%s, resp *pb.%s) error {
-	return c.Call(ctx, "%s", "%s", req, resp)
+	return c.Call(ctx,  "%s", req, resp)
 }
 `
 
 func genClient(protoInfo *gencode.ProtoFileInfo) (string, string) {
 	data := fmt.Sprintf(formatPart1, protoInfo.ModuleName, protoInfo.ServiceName, protoInfo.ServiceName,
-		protoInfo.ServiceName, protoInfo.ServiceName)
+		protoInfo.ServiceName, protoInfo.ServiceName, protoInfo.ServiceName)
 
 	for _, methodInfo := range protoInfo.Methods {
-		data += fmt.Sprintf(formatFunc, protoInfo.ServiceName, methodInfo.MethodName,
+		data += fmt.Sprintf(formatFunc,  methodInfo.MethodName,
 			methodInfo.InputType, methodInfo.OutputType, protoInfo.ServiceName, methodInfo.MethodName)
 	}
 	return protoInfo.ModuleName + "client.go", data
