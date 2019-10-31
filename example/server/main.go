2d5c4cf520122ccd5aa832b5f77c4f7699c87c5f
hillguo <wyguoqingshan@163.com>
2019-10-31 19:38:33 +0800

    ci

--- example/server/main.go
+++ example/server/main.go
@@ -7,19 +7,44 @@ import (
 	"github.com/hillguo/sanrpc/config"
 	"github.com/hillguo/sanrpc/service"
 	log "github.com/hillguo/sanlog"
+	"reflect"
 )
 
 type Test struct {
 
 }
 
-func (t *Test) Add(ctx context.Context, a *example.Req , b *example.Resq) error  {
+
+func (t *Test) Del(ctx context.Context, a *example.Req , b *example.Resq) error  {
 	log.Info("11")
 
 	return nil
 }
 
+type Test2 struct {
+
+}
+
+func (t *Test2) Add(ctx context.Context, a *example.Req , b *example.Resq) error  {
+	log.Info("11")
+
+	return nil
+}
+
+type ATest struct {
+	Test
+	Test2
+}
+
+func (t *ATest) CAdd(ctx context.Context, a *example.Req , b *example.Resq) error  {
+	log.Info("CAdd")
+	return nil
+}
+
 func main(){
+	t := reflect.TypeOf(&ATest{})
+	log.Info(t.NumMethod())
+
 	svr := sanrpc.NewServer()
 	c := config.GetConfig()
 	log.Info(c)
@@ -27,7 +52,8 @@ func main(){
 	log.Info(ss)
 	for _,s := range ss {
 		if s.Name() == "test" {
-			s.Register(&Test{})
+			err := s.Register(&ATest{})
+			log.Info(err)
 		}
 		svr.AddService(s.Name(), s)
 	}
